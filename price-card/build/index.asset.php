16f779e2ca334661d0da419379a11470453755df
TomorchandraRoy <tomorchandraroy884@gmail.com>
2025-02-01 13:04:01 +0600

    Plugin Icon change

--- price-card/build/index.asset.php
+++ price-card/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-blob', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-i18n'), 'version' => 'b888a08589f3f5749e71');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-blob', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-i18n'), 'version' => 'c038e1b8013afcc5c7ff');
